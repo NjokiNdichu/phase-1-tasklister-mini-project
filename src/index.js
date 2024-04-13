0c5aa456f2546e083f3a7ed0a92244462b40244e
Njoki Ndichu <rejoicenjoki0@gmail.com>
2024-04-13 19:25:48 +0300

    tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const inputForm= document.querySelector("form")
+  inputForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const input= document.querySelector("#new-task-description")
+    const taskDescription = input.value
+    console.log(taskDescription)
+    const taskList = document.querySelector("#tasks")
+    const newItem = document.createElement("li");
+    newItem.textContent = taskDescription;      
+    taskList.appendChild(newItem);
+    input.value = ''
+  })
+
 });
+
